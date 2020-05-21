d9836681696d5f7f7290f0082d52b102c114ea15
mesmacosta <mesmacosta@gmail.com>
2020-05-21 10:46:33 -0300

    Bump version: 0.2.1 → 0.2.2

--- src/datacatalog_fileset_exporter/__init__.py
+++ src/datacatalog_fileset_exporter/__init__.py
@@ -4,6 +4,6 @@ from .datacatalog_fileset_exporter_cli import main  # noqa: F401
 
 __author__ = """Marcelo Miranda"""
 __email__ = 'mesmacosta@gmail.com'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 __all__ = ('main')
